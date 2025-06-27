b7b3d22244cfcc3a291d077e09c8c70c8d13b2a8
pdhuy123 <pdhuy2019@gmail.com>
2025-06-27 15:29:39 +0700

    add files to server-python

---
+++ server_python/crawler/crawl_main.py
@@ -0,0 +1,7 @@
+# File chính để crawl dữ liệu
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
---
+++ server_python/inference/infer_main.py
@@ -0,0 +1,7 @@
+# File chính để inference với mô hình đã train
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
